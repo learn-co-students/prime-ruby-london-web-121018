c60aa9052512876d67a6743439ac1bd26c032bc9
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-19 20:27:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,34 @@
 # Add  code here!
+
+# def prime?(num)
+#   primearry = (2..(num-1)).to_a 
+#   isitprime2 = true; 
+  
+#   primearry.each do |d|
+#     isitprime1 = !(num % d == 0)
+#     isitprime2 = isitprime2 || isitprime1 ;
+#   end
+#     isitprime2
+#   end 
+
+def prime? (num)
+  if num < 0
+    num = num * -1  
+  end 
+  if num == 1 || num == 0
+    num = 8
+  end 
+  isitprime = true; 
+  primearry = (2..(num-1)).to_a 
+  primearry.each do |d|
+    isitprime = !(num % d == 0)
+    break if isitprime == false 
+  end 
+  isitprime
+end 
+  
+
+
+
+  
+  
