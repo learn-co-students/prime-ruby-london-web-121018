731e560f3c842a3cabdf175e35b108e50eb8faf4
I_AM Elaine <elaine.onb@gmail.com>
2018-12-04 22:54:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  if int <= 1 
+    return false
+  end
+  if int <= 3
+    return true
+  end
+    
+  if int % 2 == 0 || int % 3 == 0 
+    return false
+  else
+    return true
+  end
+end
